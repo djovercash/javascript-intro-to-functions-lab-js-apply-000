bc74fd69da4a50dc9b280375ddc3d582c06f3b92
Drew Overcash <drewovercash@gmail.com>
2017-10-29 14:43:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(whisper(string));
 }
+
+function sayHiToGrandma(string) {
+  if (whisper(string)) {
+    console.log("I can't hear you!")
+  } else if (shout(string)) {
+    console.log("YES INDEED!")
+  } else if (string === "I love you, Grandma.") {
+    console.log("I love you, too.")
+  }
+}
